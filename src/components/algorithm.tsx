60cfc8aa0eb1f6cf7bfa56ea977769511753739b
Jovan <jamc015lol@gmail.com>
2022-09-12 19:18:59 +0800

    Update

--- src/components/algorithm.tsx
+++ src/components/algorithm.tsx
@@ -1,6 +1,67 @@
 import React from "react";
+/**
+ * Structure in here
+ */
+
+interface Options {
+    validate:(str:string) => boolean;
+    func:(str:string) => string;
+}
+
+type methodType = {[key:string]:Options};
+
+const methods:methodType = {
+    PLDTHOMEDSL:  {
+        validate:(str:string):boolean => {
+            // Regex here to test if its qualified to process here
+            return true;
+        },
+        func:(str:string):string => {
+            return String();
+        }
+    },
+    PLDTHOMEFIBR:  {
+        validate:(str:string):boolean => {
+            // Regex here to test if its qualified to process here
+            return true;
+        },
+        func:(str:string):string => {
+            return String();
+        }
+    },
+    PLDTFIBR: {
+        validate:(str:string):boolean => {
+            // Regex here to test if its qualified to process here
+            return true;
+        },
+        func:(str:string):string => {
+            return String();
+        }
+    },
+    PLDTWIFI: {
+        validate:(str:string):boolean => {
+            // Regex here to test if its qualified to process here
+            return true;
+        },
+        func:(str:string):string => {
+            return String();
+        }
+    }
+}
+
+const get_method:Function = (str:string):string => {
+    return String();
+}
 
 
 export default (str:string):string[] => {
-    return ['324'];
+    const mthd:string = get_method(str);
+    const pwds:string[] = [];
+    
+    // try in every methods
+    for(const [k, v] of Object.entries(methods))
+        if(v.validate(str))
+            pwds.push(v.func(str))
+    
+    return pwds;
 }
