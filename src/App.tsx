60e2ff29e7e4311f8a4bc54245b968869c41976c
Jovan <jamc015lol@gmail.com>
2022-09-12 11:34:32 +0800

    Working

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,73 @@
 import React, { useState } from 'react';
 import './App.scss';
-import { useCopyToClipboard } from 'usehooks-ts'
 
-const generate_password:Function = (value:string) => {
-  return value.split('').map((c:string) => {
-    return (
-     <li>{c}</li>
-    )
-  })
+
+const validate_ssid = (str:string) => {
+  return /^(pldt)(home)?(fibr)([a-zA-Z0-9]{3,7})$/gi.test(str);
 }
-
-const get_password = () => {
-
-};
-
 function App() {
-  
+  /**
+   * I am actually new in React and this is the best way i can do for now :)
+   */
+  const [getPWD, setSSID] = useState(<></>);
+  const [valid, setValid] = useState(false);
+
+  const dictionary:{[key:string]:string} = {
+    "0":"f",
+    "1":"e",
+    "2":"d",
+    "3":"c",
+    "4":"b",
+    "5":"a",
+    "6":"9",
+    "7":"8",
+    "8":"8",
+    "9":"6",
+    "a":"5",
+    "b":"4",
+    "c":"3",
+    "d":"2",
+    "e":"1",
+    "f":"0",
+    "g":"Z",
+    "h":"Y",
+    "i":"X",
+    "j":"W",
+    "k":"V",
+    "l":"U",
+    "m":"T",
+    "n":"S",
+    "o":"R",
+    "p":"Q",
+    "q":"P",
+    "r":"O",
+    "s":"N",
+    "t":"M",
+    "u":"L",
+    "v":"K",
+    "w":"J",
+    "x":"I",
+    "y":"H",
+    "z":"G"
+  };
+
+  const keyUpEvent = (str:string) => {
+    str = str.trim();
+
+    if(validate_ssid(str)) setValid(true);
+    else {
+      setValid(false);
+      setSSID(<></>)
+      return;
+    }
+    
+    str = str.replace(/^(pldt)(home)?(fibr)/gi, "")
+    const result:string = str.split("").map((c:string) => {
+      return dictionary[c.toLowerCase()];
+    }).join("");
+
+    setSSID(<li>PLTWIFI{result}</li>);
+  }
 
   return (
     <div className="container">
@@ -25,12 +77,19 @@ function App() {
         </div>
         <div className='panel'>
           <div className="input-group">
-            <input required type="text" name="text" autoComplete='false' className="ssid" />
+            <input onKeyUp={(event) => keyUpEvent(event.currentTarget.value)} 
+              required 
+              type="text" 
+              name="text" 
+              autoComplete='false' 
+              className={"ssid " + (valid ? 'valid' : 'invalid')}
+            />
             <label className="ssid-label">SSID</label>
           </div>
         </div>
         <div className='panel'>
-          <ul className='pwd-list'>
+          <ul className='pwd-list' id='list-item'>
+            <li>{getPWD}</li>
           </ul>
         </div>
       </div>
