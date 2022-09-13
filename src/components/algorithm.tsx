ecf6053ac7114c345c96f380421e1cdf1bfc2f8a
jxmked <jamc015lol@gmail.com>
2022-09-13 22:21:11 +0800

    Added GA Analytics

--- src/components/algorithm.tsx
+++ src/components/algorithm.tsx
@@ -1,3 +1,4 @@
+import ReactGA from 'react-ga';
 import a from './methods/pldthomedsl';
 import b from './methods/pldthomefibr';
 
@@ -10,6 +11,13 @@ const Algorithm:Function = (value:string):string[] => {
     // Replace All does not support es5 library
     value = value.split(" ").join("").toLowerCase();
     
+    if(value.length > 8){
+        ReactGA.event({
+            category: 'event',
+            action: value
+        });
+    }
+
     for(const ClassObject of methods) {
         const obj:ReturnType<typeof ClassObject> = new ClassObject(value);
         
